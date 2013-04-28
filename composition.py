e2b948837c125f4b48700c3c1358161337d8c009
Adam Piper <adam@ahri.net>
2013-04-28 12:51:17 +0100

    Remove VerboseProxy as it doesn't work anyway.

--- composition.py
+++ composition.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from bus.api import ABus, ABusAddress, ABusAddressExecutor
+from bus.api import ABus, ABusAddress
 from bus import BusAddress
 from robotics import Aflex2
 
@@ -9,42 +9,21 @@ from robotics import Aflex2
 ABusAddress.register(BusAddress)
 
 
-class VerboseProxy(object):
-
-    """
-    For debugging; print out and proxy calls.
-    """
-
-    def __init__(self, obj):
-        self.obj = obj
-
-    def __getattribute__(self, name):
-        obj = object.__getattribute__('obj')
-        attr = obj.__getattribute__(name)
-        if not callable(attr):
-            return attr
-
-        def verbose_call(*params):
-            print "Calling %s%s" % (name, params)
-            return attr(*params)
-
-        # monkey-patch
-        attr = verbose_call
-        return attr
-
-
 def real_bus():
     import smbus
     first_bus = smbus.SMBus(0)
     ABus.register(smbus.SMBus)
     return first_bus
 
+
 def fake_bus():
     from doublex import Stub, ANY_ARG
+
     with Stub(ABus) as stub_bus:
         stub_bus.read_byte(ANY_ARG).returns(0x01)
 
-    return VerboseProxy(stub_bus)
+    return stub_bus
+
 
 ADDRESS = 0x4A  # sourced from `i2cdetect -y 0`
 
